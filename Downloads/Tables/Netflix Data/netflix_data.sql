df02c03ca7ce00b362088c3ece28f91698991fc4
Vernyuy <vernyuy@github.com>
2025-02-12 21:11:06 +0000

    Removed SQL files, assets, and README

---
+++ Downloads/Tables/Netflix Data/netflix_data.sql
@@ -0,0 +1,13 @@
+CREATE TABLE netflix_data (
+    show_id SERIAL PRIMARY KEY,
+    type TEXT,
+    title TEXT,
+    director TEXT,
+    country TEXT,
+    date_added DATE,
+    release_year INT,
+    rating TEXT,
+    duration TEXT,
+    listed_in TEXT,
+);
+
