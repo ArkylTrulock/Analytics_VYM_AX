d89d5a7f230cd1c7da0b56816f14ee1be3e2a411
Vernyuy <vernyuy@github.com>
2025-03-24 17:45:55 +0000

    Ignored Downloads folder

--- Downloads/Tables/Netflix Data/COPY netflix_data.sql
+++
@@ -1,3 +0,0 @@
-COPY netflix_data
-FROM 'C:\Users\tailb\Data Science\PostgreSQL Projects\CSV files\netflix1.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
--- Downloads/Tables/Netflix Data/netflix_database.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE netflix_database;
